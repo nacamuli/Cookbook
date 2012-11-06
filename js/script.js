2f00f731c3925bdd43e20b551c4506045f16531a
Daniel Nacamuli <danielnacamuli@Daniel-Nacamulis-MacBook-Pro.local>
2012-11-06 18:37:29 +0000

    iscroll
    Added iscroll to test performance comapred ot the web view

--- js/script.js
+++ js/script.js
@@ -1,119 +1,43 @@
+// JavaScript Document
+var my2way;
+var myScroll;
+var myScroll2;
+var myScroll3;
 
+function loaded() {
+	my2way = new TWIS('#wrapper2way');
+	myScroll = new iScroll('wrapper1',{vScrollbar: false,bounce:false,snap: 'li' });
+    myScroll2 = new iScroll('wrapper2',{vScrollbar: false,bounce:false,snap: 'li' });
+    myScroll3 = new iScroll('wrapper3',{vScrollbar: false,bounce:false,snap: 'li' });
+		$('.wrapper').css("-webkit-transform","translate(-00px, -30px)");
+		console.log($('.wrapper').css("-webkit-transform"));
+matrix = matrixToArray($(".wrapper").css("-webkit-transform"));
+console.log(matrix);
+/*
+	// Test scrollY and scrollTorecipe
+	setTimeout(function () {
+		myScroll.scrollTorecipe(3, 500);
+	}, 1000);
 
+	setTimeout(function () {
+		myScroll.scrollY(-1000, 1500);
+	}, 2000);
+*/
 
-var w0,w1,w2;
-var ws=new Array();
-$(document).ready(function(){
-   w0=$('#w0');
-   w1=$('#w1');
-   w2=$('#w2');
-   ws=[w0,w1,w2];
-   buildWheels();
-   
-   /*
-   Orientation
-   */
-
-   
-   
-   /*
-   Set Scroll
-   */
-   document.addEventListener("touchmove", function(evt){
-		evt.preventDefault();
-	}, false);   
-   	var container = document.getElementById("w0");
-	container.addEventListener("touchmove", function(evt){
-		evt.stopPropagation();
-	}, false);
-	   container = document.getElementById("w1");
-	container.addEventListener("touchmove", function(evt){
-		evt.stopPropagation();
-	}, false);
-	container = document.getElementById("w2");
-	container.addEventListener("touchmove", function(evt){
-		evt.stopPropagation();
-	}, false);
-});//Docuemnt.ready()
-
-
-
-
-function buildWheels(){
-	for(var i=0;i<3;i++){
-	for(var j=0;j<20;j++){
-		$('<img src="img/star.svg"/>').appendTo(ws[i]);
-
-	}
-	}
+	// Free some mem
+	window.removeEventListener('load', loaded, false);
 }
-window.onresize = function(event) {
- setpos();
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+window.addEventListener('load', loaded, false);
+
+
+function moving(column){
+	console.log("moving:"+column.y);
+	$('.wrapper').css('y',-70);
 }
 
-function setpos(){
-readDeviceOrientation();
+function matrixToArray(matrix) {
+var yy= matrix.substr(7, matrix.length - 8).split(', ');
+var yyy=yy[5];
+return yyy;
 }
-
-console.log(w);
- 
- 
- 
-function closewheels(){
-//$('#wheels').animate({ marginLeft: '-500' },300,'linear',true);
-//$('#display').animate({ marginLeft: '-500'  },300,'linear',true);
-$('#wheels').animate({ width: '0' },300,'linear',true);
-} 
-
-function openwheels(){
-	
-	
-}
- 
- 
- var numOrientationChanges = 0,
-    lastOrientation,
-    startTime = + new Date(),
-    timer;
-                 		
-function readDeviceOrientation() {
-
-    var orientationLabel,
-    	curTime = + new Date() - startTime;
-                 		
-    if (Math.abs(window.orientation) === 90) {
-        // Landscape
-    document.getElementById("display").innerHTML = "lllllllll";
-    closewheels();
- 
-    } else {
-    	// Portrait
-    	document.getElementById("display").innerHTML = "pppppppppp";
- 
-    }
-    
-    if (lastOrientation !== window.orientation) {
-        
-        if (numOrientationChanges === 0) {
-            document.getElementById("first").innerHTML = 
-                orientationLabel + " (" + curTime + " ms)";
-        } else if (numOrientationChanges === 1) {
-            document.getElementById("second").innerHTML = 
-                orientationLabel + " (" + curTime + " ms)";
-            clearInterval(timer);
-        }
-        lastOrientation = window.orientation;
-        numOrientationChanges += 1;
-    }
-}
-
-window.onorientationchange = readDeviceOrientation;
-
-window.onload = function () {
-	readDeviceOrientation();
-	document.getElementById("display").innerHTML = 
-		(+ new Date() - startTime) + " ms";
-};
-
-readDeviceOrientation();
-timer = setInterval(readDeviceOrientation, 1/60);
